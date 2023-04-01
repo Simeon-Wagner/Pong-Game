f94368a2f837f7d21bc3d63c68dd17629a7f9d3c
User01name <02goto1bif@hft-stuttgart.de>
2023-04-01 22:27:39 +0200

    Complete ball movement, implement player controls (of keyboard), implement losing dialog with retry-option.

--- lib/brick.dart
+++ lib/brick.dart
@@ -3,19 +3,21 @@ import 'package:flutter/material.dart';
 class MyBrick extends StatelessWidget {
   final x;
   final y;
+  final width;
+  final thisIsPlayer;
 
-  MyBrick ({this.x, this.y});
+  MyBrick ({this.x, this.y, this.width, this.thisIsPlayer});
   
   @override
   Widget build(BuildContext context){
     return Container(
-      alignment: Alignment(x,y),
+      alignment: Alignment((2*x+width)/(2-width),y),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: Container(
-          color: Colors.white,
+          color: thisIsPlayer? Colors.blue[300] : Colors.red[400],
           height: 20,
-          width: MediaQuery.of(context).size.width / 5,
+          width: MediaQuery.of(context).size.width * width/2,
         )
       ),
     );
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pong_flutter_game/coverscreen.dart';
 import 'brick.dart';
 import 'ball.dart';
@@ -12,79 +13,202 @@ class HomePage extends StatefulWidget {
  _HomePageState createState() => _HomePageState();
 }
 
-enum direction{ UP, DOWN}
+enum direction{ UP, DOWN, LEFT, RIGHT}
+
+const Color primaryMenuColor = Colors.teal;
+Color? secondaryMenuColor = Colors.teal[50];
 
 class _HomePageState extends State<HomePage> {
   // Ball variables
   double ballX = 0;
   double ballY = 0;
 
+  double playerX = 0;
+  double brickWidth = 0.4;
+
+  double enemyX = 0;
+
   //initial direcion of the ball is downwards
-  var ballDirection = direction.DOWN;
+  var ballYDirection = direction.DOWN;
+  var ballXDirection = direction.LEFT;
 
   //Game settings
   bool gameHasStarted = false;
 
   void startGame() {
-    gameHasStarted = true;
+    //check if game is already running
+    if(gameHasStarted){
+      return;
+    }
+
+    setState(() {
+      gameHasStarted = true;
+    });
     Timer.periodic(Duration(milliseconds: 10), (timer) {
       //update direction
       updateDirection();
-
-      //move ball
+     //move ball
       moveBall();
+
+      moveEnemy();
+      //
+      if(isPlayerDead()){
+        timer.cancel();
+        _showDialog();
+      }
 
     });
   }
 
+  void resetGame(){
+    Navigator.pop(context, true);
+    setState(() {
+      gameHasStarted = false;
+      ballX = 0;
+      ballY = 0;
+      playerX = -0.2;
+      enemyX = -0.2;
+    });
+  }
   void updateDirection(){
-    if(ballY >= 0.9){
-      ballDirection = direction.UP;
-    } else if(ballY <= -0.9) {
-      ballDirection = direction.DOWN;
+    if(ballY >= 0.9 && ballX >= playerX && ballX <= playerX + brickWidth){
+      ballYDirection = direction.UP;
+    } else if(ballY <= -0.9 && ballX >= enemyX && ballX <= enemyX+brickWidth) {
+      ballYDirection = direction.DOWN;
+    }
+
+    if(ballX >= 1){
+      ballXDirection = direction.LEFT;
+    } else if(ballX <= -1) {
+      ballXDirection = direction.RIGHT;
     }
   }
 
   void moveBall(){
-    if(ballDirection == direction.DOWN){
-      ballY += 0.01;
-    }else if(ballDirection == direction.UP){
-      ballY -= 0.01;
+    setState(() {
+      if(ballYDirection == direction.DOWN){
+        ballY += 0.01;
+      }else if(ballYDirection == direction.UP){
+        ballY -= 0.01;
+      }
+
+      if(ballXDirection == direction.RIGHT){
+        ballX += 0.01;
+      }else if(ballXDirection == direction.LEFT){
+        ballX -= 0.01;
+      }
+    });
+  }
+
+  void moveLeft(){
+    setState(() {
+      playerX -= 0.1;
+      if(playerX < -1){
+        playerX = -1;
+      }
+    });
+  }
+
+  void moveRight(){
+    setState(() {
+      playerX += 0.1;
+      if(playerX + brickWidth > 1) {
+        playerX = 1 - brickWidth;
+      }
+    });
+  }
+
+  void moveEnemy(){
+    enemyX = ballX-0.2;
+  }
+
+  bool isPlayerDead(){
+    if(ballY >=1){
+      return true;
+    } else {
+      return false;
     }
   }
 
 
+  void _showDialog(){
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context){
+          return AlertDialog(
+            backgroundColor: secondaryMenuColor,
+            title: const Center(
+                child:Text(
+                  "You have lost!",
+                  style: TextStyle(color: primaryMenuColor),
+                )
+            ),
+            actions: [
+              GestureDetector(
+                onTap: resetGame,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child:Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(7),
+                    color: primaryMenuColor,
+                    child: const Text(
+                      "Play Again",
+                        style: TextStyle(
+                          color: Colors.white,
+                        )
+                    ),
+                  )
+                )
+              )
+            ],
+          );
+        });
+  }
+  
   @override
   Widget build(BuildContext context) {
-   
-      return GestureDetector(
-        onTap: startGame,
-        child: Scaffold(
-          backgroundColor: Colors.grey[900],
-          body: Center(
-            child: Stack(
-            children: [
-              //tap to play
-              CoverScreen(
-                gameHasStarted: gameHasStarted
-              ),
-              // top brick
-              MyBrick( x: 0, y: -0.9),
-              // bottom brick
-              MyBrick( x: 0, y: 0.9),
+      return RawKeyboardListener(
+        focusNode:FocusNode(),
+        autofocus:true,
+        onKey: (event) {
+          if(event.isKeyPressed(LogicalKeyboardKey.arrowLeft)){
+            moveLeft();
+          } else if(event.isKeyPressed(LogicalKeyboardKey.arrowRight)){
+            moveRight();
+          }
+        },
 
-              //ball
-              MyBall(
-                x: ballX,
-                y: ballY,
-              )
-            
-            ]
-          ),
-        ),
-      )
-    );
+        child: GestureDetector(
+          onTap: startGame,
+          child: Scaffold(
+            backgroundColor: Colors.grey[900],
+            body: Center(
+              child: Stack(
+              children: [
+                //tap to play
+                CoverScreen(
+                  gameHasStarted: gameHasStarted
+                ),
+                // top brick (enemy)
+                MyBrick( x: enemyX, y: -0.9, width: brickWidth, thisIsPlayer: false,),
+                // bottom brick (Player)
+                MyBrick( x: playerX, y: 0.9, width: brickWidth, thisIsPlayer: true,),
+
+                //ball
+                MyBall(
+                  x: ballX,
+                  y: ballY,
+                ),
+                ]
+              ),
+            ),
+          )
+        )
+      );
   }
-    
+
+
   }
 
